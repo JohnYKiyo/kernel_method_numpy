02a4cf4c8f0cf797f11d9a6d8bd4d0375e87a4e6
Yu Kiyokawa <y-kiyokawa@cq.jp.nec.com>
2019-11-01 14:29:22 +0900

    [hotfix]

--- src/kernel/KernelMean.py
+++ src/kernel/KernelMean.py
@@ -4,33 +4,6 @@ import numpy as np
 import pandas as pd
 
 class KernelMean():
-    """
-    input: x is pandas dataframe
-    """
-    def __init__(self, x, sigma='median', weights=None):
-        if isinstance(x,pd.DataFrame):
-            self.x = x
-        else:
-            self.x = pd.DataFrame(x)
-        
-        self.sigma = sigma
-        if isinstance(sigma,str):
-            self.sigma = get_band_width(x.values,sigma)
-        
-        self.p = np.full(len(x),1./len(x))
-        if weights is not None:
-            self.p = weights/np.sum(weights)
-                
-        self.mu_p = lambda val: self._compute_kernel_from_samples(val)
-      
-    def _compute_kernel_from_samples(self, val):
-        kernel = gauss_kernel(self.sigma)
-        return_val = 0
-        for sample, p in zip(self.x.values,self.p):
-            return_val += p*kernel(val,sample)
-        return return_val
-
-class KernelMean():
     """ 
     input: x is pandas dataframe
     """
