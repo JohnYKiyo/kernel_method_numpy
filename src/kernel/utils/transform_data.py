258cbcdbb01ee1af1bffa4ed0f20ad4e7cff8107
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-07-23 06:57:36 +0000

    add transform data

---
+++ src/kernel/utils/transform_data.py
@@ -0,0 +1,27 @@
+from jax.config import config; config.update("jax_enable_x64", True)
+import jax
+import jax.numpy as np
+from jax import jit, vmap
+
+def transform_data(x):
+    """data transform
+
+    Args:
+        x (array-like): transform input data into 2d-array of shape (N, n_feature).
+
+    Raises:
+        ValueError: [description]
+
+    Returns:
+        array-like : [description]
+
+    """
+    if isinstance(x,np.ndarray):
+        if len(x.shape)==1:
+            return np.atleast_2d(x.astype(np.float64)).T
+        else:
+            return np.atleast_2d(x.astype(np.float64))
+    elif isinstance(x,list):
+        return transform_data(np.array(x))
+    else:
+        raise ValueError("Cannot convert to numpy.array")
