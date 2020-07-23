1640b127b8c5891074d75deb3f01c98d6666c197
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-07-23 07:12:40 +0000

    temporary change

--- src/kernel/__init__.py
+++ src/kernel/__init__.py
@@ -1,5 +1,1 @@
 from . import utils
-from . import sampling
-from .KernelMean import KernelMean as Mean
-from .KernelABC import KernelABC as ABC
-from .sampling.KernelHerding import KernelHerding as Herding
