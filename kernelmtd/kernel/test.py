891e27d25b56d57e7c2f960b8fdf607eb75df6be
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-10-27 06:41:40 +0000

    [update]speed test and equivalent function test

--- kernelmtd/kernel/test.py
+++ kernelmtd/kernel/test.py
@@ -38,6 +38,7 @@ def main():
     test_nu(1.5)
     test_nu(2.5)
     test_nu(np.inf)
+    #test_equivalent_function()
 
 if __name__ == '__main__':
     import sys
@@ -49,5 +50,60 @@ if __name__ == '__main__':
 
     import numpy as np
     import matplotlib.pyplot as plt
+    import timeit
+    from sklearn.gaussian_process.kernels import Matern as sk_Matern
+    import jax.numpy as jnp
 
     main()
+
+    #cannot use a timeit function in module.    
+    #nu=0.5
+    skmatern = sk_Matern(length_scale=1.,nu=0.5)
+    matk = MaternKernel(l=1.,nu=0.5)
+    x = jnp.atleast_2d(jnp.linspace(-10.,10.,100)).T
+    np.testing.assert_almost_equal(
+        skmatern(x,np.array([[0],[1]])),
+        matk.kde(x.reshape(100,1,1),np.array([[0],[1]]).reshape(2,1,1)).reshape(100,2))
+
+    print(f'nu=0.5 sklearn:{timeit.timeit("skmatern(x,jnp.array([[0],[1]]))", globals = globals(), number=1000)*1000:.2f} us')
+    print(f'nu=0.5 kernelmtd:{timeit.timeit("matk.kde(x.reshape(100,1,1),jnp.array([[0],[1]]).reshape(2,1,1))", globals = globals(), number=1000)*1000:.2f} us')
+    
+    #nu=1.5
+    skmatern = sk_Matern(length_scale=1.,nu=1.5)
+    matk = MaternKernel(l=1.,nu=1.5)
+    x = jnp.atleast_2d(jnp.linspace(-10.,10.,100)).T
+    np.testing.assert_almost_equal(
+        skmatern(x,np.array([[0],[1]])),
+        matk.kde(x.reshape(100,1,1),np.array([[0],[1]]).reshape(2,1,1)).reshape(100,2))
+    print(f'nu=1.5 sklearn:{timeit.timeit("skmatern(x,jnp.array([[0],[1]]))", globals = globals(), number=1000)*1000:.2f} us')
+    print(f'nu=1.5 kernelmtd:{timeit.timeit("matk.kde(x.reshape(100,1,1),jnp.array([[0],[1]]).reshape(2,1,1))", globals = globals(), number=1000)*1000:.2f} us')
+    
+    #test nu=2.5
+    skmatern = sk_Matern(length_scale=1.,nu=2.5)
+    matk = MaternKernel(l=1.,nu=2.5)
+    x = jnp.atleast_2d(jnp.linspace(-10.,10.,100)).T
+    np.testing.assert_almost_equal(
+        skmatern(x,np.array([[0],[1]])),
+        matk.kde(x.reshape(100,1,1),np.array([[0],[1]]).reshape(2,1,1)).reshape(100,2))
+    print(f'nu=2.5 sklearn:{timeit.timeit("skmatern(x,jnp.array([[0],[1]]))", globals = globals(), number=1000)*1000:.2f} us')
+    print(f'nu=2.5 kernelmtd:{timeit.timeit("matk.kde(x.reshape(100,1,1),jnp.array([[0],[1]]).reshape(2,1,1))", globals = globals(), number=1000)*1000:.2f} us')
+
+    #test nu=inf
+    skmatern = sk_Matern(length_scale=1.,nu=np.inf)
+    matk = MaternKernel(l=1.,nu=np.inf)
+    x = jnp.atleast_2d(jnp.linspace(-10.,10.,100)).T
+    np.testing.assert_almost_equal(
+        skmatern(x,np.array([[0],[1]])),
+        matk.kde(x.reshape(100,1,1),np.array([[0],[1]]).reshape(2,1,1)).reshape(100,2))
+    print(f'nu=inf sklearn:{timeit.timeit("skmatern(x,jnp.array([[0],[1]]))", globals = globals(), number=1000)*1000:.2f} us')
+    print(f'nu=inf kernelmtd:{timeit.timeit("matk.kde(x.reshape(100,1,1),jnp.array([[0],[1]]).reshape(2,1,1))", globals = globals(), number=1000)*1000:.2f} us')
+    
+    #test nu=ohter(=5)
+    skmatern = sk_Matern(length_scale=1.,nu=5)
+    matk = MaternKernel(l=1.,nu=5)
+    x = jnp.atleast_2d(jnp.linspace(-10.,10.,100)).T
+    np.testing.assert_almost_equal(
+        skmatern(x,np.array([[0],[1]])),
+        matk.kde(x.reshape(100,1,1),np.array([[0],[1]]).reshape(2,1,1)).reshape(100,2))
+    print(f'nu=other (=5.) sklearn:{timeit.timeit("skmatern(x,jnp.array([[0],[1]]))", globals = globals(), number=1000)*1000:.2f} us')
+    print(f'nu=other (=5.) kernelmtd:{timeit.timeit("matk.kde(x.reshape(100,1,1),jnp.array([[0],[1]]).reshape(2,1,1))", globals = globals(), number=1000):.2f} ms')
