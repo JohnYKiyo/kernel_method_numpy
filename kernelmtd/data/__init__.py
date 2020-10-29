31730675508c75c08635d3d99193f15c3f84e0cf
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-10-29 15:25:51 +0000

    [update] data for testing

--- kernelmtd/data/__init__.py
+++ kernelmtd/data/__init__.py
@@ -1,2 +1,4 @@
 import pandas as pd
+import numpy as np
 testdata = pd.read_csv('./kernelmtd/data/test_data1.csv')
+regression_data = np.load('./kernelmtd/data/regression.npy',allow_pickle=True).item()
