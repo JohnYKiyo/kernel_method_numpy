8b62c946c54f4cc26dbf1ab7d9e0c816fe818dbe
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-11-04 02:20:58 +0000

    [add]update init for sample_clipper

--- kernelmtd/utils/__init__.py
+++ kernelmtd/utils/__init__.py
@@ -1,5 +1,6 @@
 from .transform_data import transform_data
 from .pairwise import pairwise, gradpairwise
 from .band_width import Bandwidth
+from .sample_clipper import sample_clipper
 
-__all__ = ['transform_data', 'pairwise', 'gradpairwise', 'Bandwidth']
+__all__ = ['transform_data', 'pairwise', 'gradpairwise', 'Bandwidth', 'sample_clipper']
