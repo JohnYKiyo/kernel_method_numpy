b7c63df0c21d64fed9b44c8632ab96abc1b91406
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-11-04 04:17:15 +0000

    [add]data __init__

--- kernelmtd/data/__init__.py
+++ kernelmtd/data/__init__.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+
 absolute_path = os.path.dirname(os.path.abspath(__file__))
-testdata = pd.read_csv(absolute_path+'/test_data1.csv')
-regression_data = np.load(absolute_path+'/regression.npy', allow_pickle=True).item()
+testdata = pd.read_csv(absolute_path + '/test_data1.csv')
+regression_data = np.load(absolute_path + '/regression.npy', allow_pickle=True).item()
